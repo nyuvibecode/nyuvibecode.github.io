82b796db97d37ef1e7fd97376169d820864a9c4a
Meet Udeshi <mudeshi1209@gmail.com>
2024-09-17 11:35:08 -0400

    Add sorting in the leaderboard js loader, also display percentage
    Add loader spinner and switch in js
    
    Keeping template updates for now, can be removed later.

--- js/load_leaderboard.js
+++ js/load_leaderboard.js
@@ -6,26 +6,40 @@ function loadLeaderboard() {
     fetch('../data/leaderboard.json')
         .then(response => response.json())
         .then(data => {
-            updateLeaderboardTable(data.submissions);
+            updateLeaderboardTable(data);
             updateDatasetSummary(data.dataset);
         })
         .catch(error => console.error('Error loading the leaderboard:', error));
 }
 
-function updateLeaderboardTable(submissions) {
-    const leaderboardTableBody = document.querySelector('#leaderboard table tbody');
-    leaderboardTableBody.innerHTML = ''; // Clear existing rows
+function updateLeaderboardTable(data) {
+    const leaderboardLoader = document.querySelector('#leaderboard_loader');
+    const leaderboardTableBody = document.querySelector('#leaderboard tbody');
 
-    submissions.forEach((item, index) => {
+    if (!leaderboardTableBody.classList.contains('is-hidden')) {
+        leaderboardTableBody.classList.add('is-hidden');
+    }
+    if (leaderboardLoader.classList.contains('is-hidden')) {
+        leaderboardLoader.classList.remove('is-hidden');
+    }
+
+    leaderboardTableBody.innerHTML = ''; // Clear existing rows
+    data.submissions.sort((a, b) => {return a.solved - b.solved;}).reverse();
+
+    data.submissions.forEach((item, index) => {
         const row = document.createElement('tr');
+        const score = (item.solved / data.dataset.total * 100).toFixed(2);
         row.innerHTML = `
             <td>${index + 1}</td>
             <td>${item.name}</td>
             <td>${item.model}</td>
-            <td>${item.solved}</td>
+            <td>${score}%</td>
             <td>${item.logs}</td>
             <td>${item.link}</td>
         `;
         leaderboardTableBody.appendChild(row);
     });
+
+    leaderboardTableBody.classList.remove('is-hidden');
+    leaderboardLoader.classList.add('is-hidden');
 }

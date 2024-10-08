9de5a6436e9b141e6f94fbe957762ae666fdf1ad
Meet Udeshi <mudeshi1209@gmail.com>
2024-10-08 14:05:08 -0400

    Dataset name change

--- js/load_leaderboard.js
+++ js/load_leaderboard.js
@@ -16,7 +16,9 @@ const modelPrettyName = new Map();
 modelPrettyName.set("gpt-4-0125-preview", "GPT 4");
 modelPrettyName.set("gpt-4-1106-preview", "GPT 4");
 modelPrettyName.set("gpt-3.5-turbo-1106", "GPT 3.5");
+modelPrettyName.set("gpt-4o", "GPT 4o");
 modelPrettyName.set("claude-3-haiku-20240307", "Claude 3 Haiku");
+modelPrettyName.set("claude-3.5-sonnet-20240620", "Claude 3.5 Sonnet");
 
 function getModelPretty(model) {
     if (modelPrettyName.has(model))
